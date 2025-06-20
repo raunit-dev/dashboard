f167caae64fe1bfb03ceaccfceaec91f6926a288
raunit-dev <raunitjaiswal510@gmail.com>
2025-06-20 22:49:26 +0530

    fixed a bug

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -5,7 +5,6 @@ import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
 import { usePathname, useSearchParams } from 'next/navigation';
-import { createPageURL } from 'next/navigation';
 
 
 export default function Pagination({ totalPages }: { totalPages: number }) {

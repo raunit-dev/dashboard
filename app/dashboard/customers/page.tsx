2fc0fde6bd6a9e1abafed04459953e4d5935db9b
raunit-dev <raunitjaiswal510@gmail.com>
2025-07-08 15:56:17 +0530

    fixed a type bug

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,22 +1,31 @@
-
-import { fetchFilteredCustomers } from '@/app/lib/data';
-import CustomersTable from '@/app/ui/customers/table';
 import { lusitana } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
-import { FormattedCustomersTable } from '@/app/lib/definitions';
-
-export default async function Page({
-  searchParams,
-}: {
-  searchParams?: {
+import {
+  FormattedCustomersTable,
+} from '@/app/lib/definitions';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { Metadata } from 'next';
+import CustomersTable from '@/app/ui/customers/table';
+ 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+ 
+export default async function Page(props: {
+  searchParams?: Promise<{
     query?: string;
-  };
+  }>;
 }) {
+  const searchParams = await props.searchParams;
   const query = searchParams?.query || '';
   const customers = await fetchFilteredCustomers(query);
+
+
+ 
   return (
     <div className="w-full">
       <CustomersTable customers={customers} />
     </div>
   );
 }
+
